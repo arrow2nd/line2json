b0530a41c2c2a79891b51d16b626dc69e94045ce
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-09-18 12:45:44 +0900

    test: テストファイルを移動

--- mod_test.ts
+++ mod_test.ts
@@ -1,17 +1,17 @@
 import { assertEquals } from "https://deno.land/std@0.111.0/testing/asserts.ts";
-import { toJson } from "../mod.ts";
+import { toJson } from "./mod.ts";
 
-const expected = Deno.readTextFileSync("tests/expected.txt");
+const expected = Deno.readTextFileSync("./testdata/expected.txt");
 
 Deno.test("Parse LINE Talk File (mobile)", () => {
-  const text = Deno.readTextFileSync("tests/talk-mobile.txt");
+  const text = Deno.readTextFileSync("./testdata/talk-mobile.txt");
   const json = toJson(text);
 
   assertEquals(json, expected);
 });
 
 Deno.test("Parse LINE Talk File (PC)", () => {
-  const text = Deno.readTextFileSync("tests/talk-pc.txt");
+  const text = Deno.readTextFileSync("./testdata/talk-pc.txt");
   const json = toJson(text);
 
   assertEquals(json, expected);
